8bcdde77d734154db5be3a1029ad2a68397ebba3
John Torres <john.torres.hurtado@gmail.com>
2017-09-19 20:47:44 -0700

    CCN

--- js/script.js
+++ js/script.js
@@ -1,65 +1,24 @@
-var table = {
-  headers: {
-    method:  '<td>METHOD</td>',
-    ratio:   '<td>RATIO</td>',
-    savings: '<td>SAVINGS</td>'
-  },
-  compression: {
-    method:  '<td>Compression</td>',
-    ratio:   '<td>1.5 : 1</td>',
-    savings: '<td>202.03 TB</td>',
-    class:   '.compress'
-  },
-  dedup: {
-    method:  '<td>Deduplication</td>',
-    ratio:   '<td>1.9 : 1</td>',
-    savings: '<td>504.60 GB</td>',
-    class:   '.dedup'
-  },
-  erasure: {
-    method:  '<td>Erasure Coding</td>',
-    ratio:   '<td>1.2 : 1</td>',
-    savings: '<td>903.73 GB</td>',
-    class:   '.erasure'
+var steps = {
+  header : `<div class="popup-header">
+            <h3 class='header title'> License Management </h3> <span></span>
+            </div>`,
+  subheader : {
+    one : '<h3>1. Cluster info</h3>',
+    two : '<h3>2. Licenses</h3>',
+    three : '<h3>3. Add-on & Review</h3>',
+    html: `<div class="popup-subheader"></div>`
   }
-}
+};
+var keys = Object.keys(steps);
 
 const pop = function(){
   $('.popup-background').toggle();
 };
 
-$(document).ready(pop);
-
-function newTableRow(){
-  for(var i=0; i<Object.keys(table).length; i++){
-    const objKey = Object.keys(table)[i];
-    $('.values').append(
-      `<tr>
-           ${table[objKey].method}
-           ${table[objKey].ratio}
-           ${table[objKey].savings}
-      </tr>`
-    );
-  }
-}
-
-function cellHighlight(){
-  for(let i=1; i<Object.keys(table).length; i++){
-    let objKey = Object.keys(table)[i];
-
-    $("tr:eq("+i+")," + table[objKey].class + "").hover(function(){
-       $(table[objKey].class).addClass('hover'),
-       $("tr:eq("+i+")").addClass('cellHover');
-     },
-     function(){
-       $(table[objKey].class).removeClass('hover');
-       $("tr:eq("+i+")").removeClass('cellHover');
-      }
-    );
-  }
-}
-$('.close, .popup-header span').click(pop);
-$('.secondary').click(pop);
-
-newTableRow();
-cellHighlight();
+$(document).ready(function(){
+  $('.popup').prepend(steps.header);
+  $('.popup-header').after(steps.subheader.html);
+  $('.popup-subheader').append(steps.subheader.one);
+  $('.popup-subheader').append(steps.subheader.two);
+  $('.popup-subheader').append(steps.subheader.three);
+});
