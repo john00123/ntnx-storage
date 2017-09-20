b9f2bfe28e8f64a76ad81c6d628aa857c4026f99
John Torres <john.torres.hurtado@gmail.com>
2017-09-20 00:16:21 -0700

    added dynamic table
    table and sums

--- js/script.js
+++ js/script.js
@@ -3,22 +3,77 @@ var steps = {
             <h3 class='header title'> License Management </h3> <span></span>
             </div>`,
   subheader : {
-    one : '<h3>1. Cluster info</h3>',
-    two : '<h3>2. sdsdsd</h3>',
-    three : '<h3>3. Add-on & Review</h3>',
-    html: `<div class="popup-subheader"></div>`
-  },
-  starter : '<tr><td>hi</td>M</tr>'
+    one :   '1. Cluster info',
+    two :   '2. License',
+    three : '3. Add-on & Review',
+    html:   `<div class="popup-subheader"></div>`
+  }
 };
 
-const pop = function(){
-  $('.popup-background').toggle();
-};
+var licenseData = {
+  thead:['NUMBER','TYPE','CCU PER UNIT', 'TOTAL CCU'],
+  number:[3, 4, 5],
+  type: ['Sockets','Hard Drive','Solid State Drive'],
+  ccper:{
+    starter:[2100,4000,4500],
+    pro:[2700,4800,6000],
+    ultimate:[3000,6000,6900]
+  }
+}
 
+
+//popup structure
 $(document).ready(function(){
   $('.popup').prepend(steps.header);
   $('.popup-header').after(steps.subheader.html);
-  $('.popup-subheader').append(steps.subheader.one);
-  $('.popup-subheader').append(steps.subheader.two);
-  $('.popup-subheader').append(steps.subheader.three);
+  $('.popup-subheader').append(
+    ` <h3>${steps.subheader.one}</h3>
+      <h3>${steps.subheader.two}</h3>
+      <h3>${steps.subheader.three}</h3>`
+    );
 });
+
+
+// table data
+for(let i=0; i<licenseData.thead.length; i++){
+  $('thead').append(`<th>${licenseData.thead[i]}</th>`);
+}
+
+function tableGen(tier){
+  $('.table-body tr').remove();
+  let sum = 0;
+  for(let i=0; i<licenseData.number.length; i++){
+    $('.table-body').append(
+      `<tr>
+         <td>${licenseData.number[i]}</td>
+         <td>${licenseData.type[i]}</td>
+         <td>${tier[i]}</td>
+         <td class='total'>${tier[i]*licenseData.number[i]}</td>
+      </tr>`
+    );
+  sum += tier[i]*licenseData.number[i];
+  }
+  
+  $('.price').html(`<p>Total <span>${sum} CCU</span></p>`);
+}
+
+function checker(){
+  if ($('#license1').prop('checked')){
+    let tier = licenseData.ccper.starter;
+    tableGen(tier);
+  }
+  if ($('#license2').prop('checked')){
+    let tier = licenseData.ccper.pro;
+    tableGen(tier);
+  }
+  if ($('#license3').prop('checked')){
+    let tier = licenseData.ccper.ultimate;
+    tableGen(tier);
+  }
+};
+
+$('#license1').not(':checked').click().change(checker);
+$('#license2').not(':checked').click().change(checker);
+$('#license3').not(':checked').click().change(checker);
+
+checker();
